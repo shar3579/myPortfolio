59ffc3984c2583492b694d5f264539fe466c1935
Sharmila P Marupuru <120716197+shar3579@users.noreply.github.com>
2025-01-18 01:04:50 +0530

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -29,7 +29,7 @@ const navigation = [
   },
   {
     name: 'LinkedIn',
-    href: 'www.linkedin.com/in/sharmila-marupuru-a85975260',
+    href: 'https://www.linkedin.com/in/sharmila-marupuru-a85975260/?_l=en_US',
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill='currentColor' viewBox='0 0 448 512' {...props}>
         <path
@@ -41,7 +41,7 @@ const navigation = [
   },
   {
     name: 'X',
-    href: '#',
+    href: 'https://x.com/sharmila091104',
     icon: (props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>) => (
       <svg fill='currentColor' viewBox='0 0 24 24' {...props}>
         <path d='M13.6823 10.6218L20.2391 3H18.6854L12.9921 9.61788L8.44486 3H3.2002L10.0765 13.0074L3.2002 21H4.75404L10.7663 14.0113L15.5685 21H20.8131L13.6819 10.6218H13.6823ZM11.5541 13.0956L10.8574 12.0991L5.31391 4.16971H7.70053L12.1742 10.5689L12.8709 11.5655L18.6861 19.8835H16.2995L11.5541 13.096V13.0956Z' />
