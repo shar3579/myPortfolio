fa3491d99f4a992c37be7a278e43beed918856d1
shar3579 <sharmila091104@gmail.com>
2025-06-03 12:16:01 +0530

    Changed footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -84,8 +84,7 @@ export default function Footer() {
           </div>
           <div className='mt-8 md:order-1 md:mt-0'>
             <p className='text-center text-xs leading-5 text-muted-foreground'>
-              &copy; {new Date().getFullYear()} Your Company. All rights
-              reserved.
+              &copy; {new Date().getFullYear()} Sharmila P Marupuru
             </p>
           </div>
         </div>
